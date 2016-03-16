5659b27bb9ecd4922f4c317984132834a79a4edf
achiko <achiko@gmail.com>
2016-03-16 17:31:23 +0400

    create repo

---
+++ contract.sol
@@ -0,0 +1,107 @@
+contract ProfileBase {
+
+    address owner;
+
+    //-- Assign contract owner 
+    function ProfileBase(){ 
+        owner = msg.sender;
+    }
+
+  	//-- Profile struct 
+    struct profile {
+
+		string name;
+		int age;
+		string speciality;
+		string twitter;
+		string github;
+		string linkedin;
+	}
+
+	//-- Assign mapping 
+	mapping (address => profile ) public profiles;
+
+	
+	function createProfile(
+			string name, 
+			int age, 
+			string speciality,  
+			string twitter, 
+			string github, 
+			string linkedin
+
+	) returns(bool res) {
+       
+       	// -- Check if  sender already exists in storage 
+       	//-- !!! Getting Type error , 
+	    
+        // if(profiles[msg.sender] != 0) {
+        // 	return fals;
+        // }
+
+        // getting error here 
+        /* Untitled2:32:12: Error: Operator != not compatible with types struct profile storage ref and int_const 0
+	        if(profiles[msg.sender] != 0) {*/
+
+	   	profile _profile = profiles[msg.sender];  
+
+		_profile.name = name;
+		_profile.age = age;
+		_profile.speciality = speciality;
+		_profile.twitter = twitter;
+		_profile.github = github;
+		_profile.linkedin = linkedin;
+
+		return true;
+
+	}
+
+
+	//-- get profile 
+	function getProfile() returns(string name, int age, string speciality,  string twitter, string github, string linkedin) {
+		
+		profile data = profiles[msg.sender];  
+		return (data.name ,  data.age  , data.speciality, data.twitter,  data.github , data.linkedin);
+
+	} 
+
+
+	//-- Update profile 
+	function updateProfile(
+			string name, 
+			int age, 
+			string speciality,  
+			string twitter, 
+			string github, 
+			string linkedin
+
+	) returns(bool res) {
+
+		if (msg.sender != owner){
+			return false;
+		}
+       
+       	//To Do: -- Check profile exists , if not exists return false 
+       
+	   	profile _profile = profiles[msg.sender];  
+
+		_profile.name = name;
+		_profile.age = age;
+		_profile.speciality = speciality;
+		_profile.twitter = twitter;
+		_profile.github = github;
+		_profile.linkedin = linkedin;
+
+		return true;
+
+	}
+
+	//-- Remove Contract From Blockhain 
+	function remove() {
+		//-- Only Owner can remove
+        if (msg.sender == owner){
+            suicide(owner);
+        }
+    }
+	
+}
